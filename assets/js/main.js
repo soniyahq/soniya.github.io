e941ba283757bf5c5a4f1ef5b102fd1fe8ade0d9
soniyahq <33133621+soniyahq@users.noreply.github.com>
2017-12-02 01:39:36 +0600

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,43 +57,5 @@
 	    }
 	});
 
-	// slider js
-
-	var carousel = document.querySelector('.carousel');
-	var container = carousel.querySelector('.carousel-container');
-	var pagination = carousel.querySelector('.carousel-pagination');
-	var bullets = [].slice.call(carousel.querySelectorAll('.carousel-bullet'));
-	var totalItems = container.querySelectorAll('.carousel-item').length;
-	var percent = (100 / totalItems);
-	var currentIndex = 0;
-
-	function next(){
-	    slideTo(currentIndex + 1);
-	}
-
-	function prev(){
-	    slideTo(currentIndex - 1);
-	}
-
-	function slideTo(index){
-	    index = index < 0 ? totalItems - 1 : index >= totalItems ? 0 : index;
-	    container.style.WebkitTransform = container.style.transform = 'translate(-' + (index * percent) + '%, 0)';
-	    bullets[currentIndex].classList.remove('active-bullet');
-	    bullets[index].classList.add('active-bullet');
-	    currentIndex = index;
-	}
-
-	bullets[currentIndex].classList.add('active-bullet');
-	carousel.querySelector('.carousel-prev').addEventListener('click', prev, false);
-	carousel.querySelector('.carousel-next').addEventListener('click', next, false);
-
-	pagination.addEventListener('click', function(e){
-	    var index = bullets.indexOf(e.target);
-	    if(index !== -1){
-	        slideTo(index);
-	    }
-	}, false);
-
-
 	
 }(jQuery));	
