7726e17e7492cbc84cbd9740062f765c3d956515
soniyahq <33133621+soniyahq@users.noreply.github.com>
2017-12-01 10:08:53 +0600

    add jarbit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,99 @@
-$(function() {
-  // This will select everything with the class smoothScroll
-  // This should prevent problems with carousel, scrollspy, etc...
-  $('.smoothScroll').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000); // The number here represents the speed of the scroll in milliseconds
-        return false;
-      }
-    }
-  });
-});
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+
+        
+
+
+    });
+
+
+    $(document).ready(function () {
+	    $(".btn-select").each(function (e) {
+	        var value = $(this).find("ul li.selected").html();
+	        if (value != undefined) {
+	            $(this).find(".btn-select-input").val(value);
+	            $(this).find(".btn-select-value").html(value);
+	        }
+	    });
+	});
+
+	$(document).on('click', '.btn-select', function (e) {
+	    e.preventDefault();
+	    var ul = $(this).find("ul");
+	    if ($(this).hasClass("active")) {
+	        if (ul.find("li").is(e.target)) {
+	            var target = $(e.target);
+	            target.addClass("selected").siblings().removeClass("selected");
+	            var value = target.html();
+	            $(this).find(".btn-select-input").val(value);
+	            $(this).find(".btn-select-value").html(value);
+	        }
+	        ul.hide();
+	        $(this).removeClass("active");
+	    }
+	    else {
+	        $('.btn-select').not(this).each(function () {
+	            $(this).removeClass("active").find("ul").hide();
+	        });
+	        ul.slideDown(300);
+	        $(this).addClass("active");
+	    }
+	});
+
+	$(document).on('click', function (e) {
+	    var target = $(e.target).closest(".btn-select");
+	    if (!target.length) {
+	        $(".btn-select").removeClass("active").find("ul").hide();
+	    }
+	});
+
+	// slider js
+
+	var carousel = document.querySelector('.carousel');
+	var container = carousel.querySelector('.carousel-container');
+	var pagination = carousel.querySelector('.carousel-pagination');
+	var bullets = [].slice.call(carousel.querySelectorAll('.carousel-bullet'));
+	var totalItems = container.querySelectorAll('.carousel-item').length;
+	var percent = (100 / totalItems);
+	var currentIndex = 0;
+
+	function next(){
+	    slideTo(currentIndex + 1);
+	}
+
+	function prev(){
+	    slideTo(currentIndex - 1);
+	}
+
+	function slideTo(index){
+	    index = index < 0 ? totalItems - 1 : index >= totalItems ? 0 : index;
+	    container.style.WebkitTransform = container.style.transform = 'translate(-' + (index * percent) + '%, 0)';
+	    bullets[currentIndex].classList.remove('active-bullet');
+	    bullets[index].classList.add('active-bullet');
+	    currentIndex = index;
+	}
+
+	bullets[currentIndex].classList.add('active-bullet');
+	carousel.querySelector('.carousel-prev').addEventListener('click', prev, false);
+	carousel.querySelector('.carousel-next').addEventListener('click', next, false);
+
+	pagination.addEventListener('click', function(e){
+	    var index = bullets.indexOf(e.target);
+	    if(index !== -1){
+	        slideTo(index);
+	    }
+	}, false);
+
+
+	
+}(jQuery));	
