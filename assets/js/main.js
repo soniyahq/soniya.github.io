1a4cbed1af493f90cd1700a005a84f62a02e20d9
soniyahq <33133621+soniyahq@users.noreply.github.com>
2018-04-05 11:31:42 +0600

    edited

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,46 @@
-(function ($) {
-"use strict";
-$('.selectpicker').selectpicker();
-
-
-
-
-
-
-})(jQuery);
+$(document).ready(function(){
+			function buttonUp(){
+				 var valux = $('.ExpInput').val(); 
+					valux = $.trim(valux).length;
+					if(valux !== 0){
+						$('.Expbtn').css('z-index','99');
+					} else{
+						$('.ExpInput').val(''); 
+						$('.Expbtn').css('z-index','-999');
+					}
+			}
+			});
+			
+			$(document).ready(function(){
+				var submitIcon = $('.ExpIcon');
+				var submitInput = $('.ExpInput');
+				var searchBox = $('.Exp-serach');
+				var isOpen = false;
+				
+				$(document).mouseup(function(){
+					if(isOpen == true){
+					submitInput.val('');
+					$('.Expbtn').css('z-index','-999');
+					submitIcon.click();
+					}
+				});
+				
+				submitIcon.mouseup(function(){
+					return false;
+				});
+				
+				searchBox.mouseup(function(){
+					return false;
+				});
+						
+				submitIcon.click(function(){
+					if(isOpen == false){
+						searchBox.addClass('Exp-serach-open');
+						isOpen = true;
+					} else {
+						searchBox.removeClass('Exp-serach-open');
+						isOpen = false;
+					}
+			});
+			
+			});
